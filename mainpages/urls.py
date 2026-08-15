a604bbf260ad7717d683a3a0db4c4d27444b1581
Faraz <farazrze18@gmail.com>
2026-08-15 11:17:30 +0330

    dynamic url

--- mainpages/urls.py
+++ mainpages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
 from . views import *
 
+app_name="mainpages"
+
 urlpatterns = [
-    path("",index),
-    path("contact",contact),
-    path("about",about)
+    path("",index,name="home"),
+    path("contact",contact,name="contact"),
+    path("about",about,name="about"),
 ]
--- my_site2026/settings.py
+++ my_site2026/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mainpages.apps.MainpagesConfig'
+    'mainpages.apps.MainpagesConfig',
 ]
 
 MIDDLEWARE = [
