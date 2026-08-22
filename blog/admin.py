7b75020df69836bdc827c7d8de5faaf95e928bb2
Faraz <141013131+farazrze@users.noreply.github.com>
2026-08-22 10:16:35 +0330

    add category

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post,Category
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -9,3 +9,4 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display="empty"
 
 admin.site.register(Post,PostAdmin)
+admin.site.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     image = models.ImageField(upload_to="blog/",default="blog/default.jpg")
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
@@ -11,6 +18,9 @@ class Post(models.Model):
     create_date = models.DateField(auto_now_add=True)
     update_date = models.DateField(auto_now=True)
     counter_view = models.IntegerField(default=0)
+    category = models.ManyToManyField(Category)
+
+
     class Meta:
         ordering = ["-create_date"]
     def __str__(self):
