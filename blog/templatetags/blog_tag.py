7a9f6564b41fd7a1642e17a7dd2961906440078a
Faraz <141013131+farazrze@users.noreply.github.com>
2026-08-23 13:22:20 +0330

    add filter tag

--- blog/templatetags/blog_tag.py
+++ blog/templatetags/blog_tag.py
@@ -1,3 +1,14 @@
 from django import template
+from blog.models import Post
 
-register = template.library
+register = template.Library()
+
+
+@register.simple_tag
+def posts():
+    return Post.objects.filter(status=1)
+
+
+@register.filter
+def snippet(value, arg=20):
+    return value[:int(arg)]
