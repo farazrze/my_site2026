cf0abbb20a4c76f01332ea34cbf9253862631fe8
Faraz <141013131+farazrze@users.noreply.github.com>
2026-08-24 10:38:03 +0330

    add category url in blog/ home , single , cat_post

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name="blog"
 urlpatterns = [
     path("",blog_home,name="home_blog"),
     path("<int:pid>",blog_single,name="single_blog"),
+    path("category/<str:cat_name>",blog_category,name="category_blog"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -14,3 +14,10 @@ def blog_single(request,pid):
     post=get_object_or_404(Post,id=pid,status=1)
     context={"post":post}
     return render(request,"blog/single.html",context)
+
+
+def blog_category(request,cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {"posts":posts}
+    return render(request,"blog/home.html",context)
