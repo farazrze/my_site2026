3ed1ffc23495d64614589ee8b62917e4c8c1bcdc
Faraz <141013131+farazrze@users.noreply.github.com>
2026-08-24 11:45:20 +0330

    second way

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("",blog_home,name="home_blog"),
     path("<int:pid>",blog_single,name="single_blog"),
     path("category/<str:cat_name>",blog_home,name="category_blog"),
+    path("author/<str:author_name>",blog_home,name="author_blog"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,12 +4,26 @@ from blog.models import Post
 
 
 
+"""     first way:
 def blog_home(request,cat_name=None):
     posts = Post.objects.filter(status=1)
     if cat_name:
         posts = posts.filter(category__name=cat_name)
     context = {'posts': posts}
     return render(request, "blog/home.html", context)
+"""
+
+# second way:
+
+def blog_home(request,**kwargs):
+    posts = Post.objects.filter(status=1)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_name') != None:
+        posts = posts.filter(author__username=kwargs['author_name'])
+    context = {'posts': posts}
+    return render(request, "blog/home.html", context)
+
 
 
 def blog_single(request,pid):
