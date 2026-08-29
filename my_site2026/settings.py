7f6cafc9aa49b41f6fb71e1532d45347e9707168
Faraz <141013131+farazrze@users.noreply.github.com>
2026-08-29 10:17:39 +0330

    add admin captcha

--- my_site2026/settings.py
+++ my_site2026/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'multi_captcha_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,6 +50,13 @@ INSTALLED_APPS = [
     'mainpages.apps.MainpagesConfig',
     'blog.apps.BlogConfig',
 ]
+
+
+# admin multicaptcha
+
+MULTI_CAPTCHA_ADMIN = {
+    'engine': 'simple-captcha',
+}
 
 
 # summernote
