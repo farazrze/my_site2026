50bddb22975db2e13318fa28a2b049987f6097cb
Faraz <141013131+farazrze@users.noreply.github.com>
2026-08-29 19:33:00 +0330

    add rss feed

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
 from . views import *
+from blog.feeds import *
 
 app_name="blog"
 
@@ -10,5 +11,5 @@ urlpatterns = [
     path("tag/<str:tag_name>",blog_home,name="tag"),
     path("author/<str:author_name>",blog_home,name="author_blog"),
     path("search/",blog_search,name="search"),
-    
+    path("rss/feeds/", LatestEntriesFeed()),
 ]
