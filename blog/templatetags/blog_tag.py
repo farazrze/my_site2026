aa4fc01cd7f619aa8a1723f862f9b732837d0316
Faraz <141013131+farazrze@users.noreply.github.com>
2026-08-29 18:12:20 +0330

    add simple tag comments count

--- blog/templatetags/blog_tag.py
+++ blog/templatetags/blog_tag.py
@@ -1,11 +1,11 @@
 from django import template
-from blog.models import Post,Category
+from blog.models import Post,Category,Comment
 
 register = template.Library()
 
 
-@register.simple_tag
-def posts():
+@register.simple_tag(name="posts")
+def function():
     return Post.objects.filter(status=1)
 
 
@@ -27,3 +27,7 @@ def category():
     for name in cat:
         cat_dic[name]= post.filter(category=name).count()
     return {"categoris":cat_dic}
+
+@register.simple_tag(name="comments_count")
+def function(pid):
+    return Comment.objects.filter(post=pid).count()
